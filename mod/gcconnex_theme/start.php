b8ac37941acb94861fed8ec88d566c8a72f0c9d6
Phanoix <phanoix@gmail.com>
2016-12-01 11:45:42 -0500

    register menu item

--- mod/gcconnex_theme/start.php
+++ mod/gcconnex_theme/start.php
@@ -10,4 +10,11 @@ elgg_register_event_handler('init', 'system', 'gcconnex_theme_init');
 
 function gcconnex_theme_init() {
     elgg_register_page_handler('hello', 'gcconnex_theme_page_handler');
+
+    //menu item for career dropdown
+    elgg_register_menu_item('site', array(
+    		'name' => 'career',
+    		'href' => '#career_menu',
+    		'text' => elgg_echo('career') . '<span class="expicon glyphicon glyphicon-chevron-down"></span>'
+    ));
 }
