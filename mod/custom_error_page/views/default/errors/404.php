adda15caa9f1ce95635d7d61c4ceea34d6248ace
steph4104 <stephanie4104@gmail.com>
2017-03-01 17:13:19 -0600

    update design error page

--- mod/custom_error_page/views/default/errors/404.php
+++ mod/custom_error_page/views/default/errors/404.php
@@ -14,12 +14,15 @@
 // we won't trust server configuration but specify utf-8
 header('Content-type: text/html; charset=utf-8');
 $vars['title'] = '404';
-$vars['body'] = 'My body';
+$vars['body'] = 'My body<br>Delete file';
+$gc_language = $_COOKIE['connex_lang'];
+
+$tokens = explode('/', $vars['current_url']);
+$str = trim(end($tokens));
+
 ?>
-<html>
+
 	<head>
-		<title><?php echo $vars['title']; ?></title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 		<style type="text/css">
 
@@ -28,34 +31,73 @@ $vars['body'] = 'My body';
 			margin:0;
 			padding:0;
 			background: #4690d6;
-			font: 80%/1.5  "Lucida Grande", Verdana, sans-serif;
+			/*font: 80%/1.5  "Lucida Grande", Verdana, sans-serif;*/
 			color: #333333;
 		}
 		p {
 			margin: 0px 0px 15px 0;
 		}
 		#elgg-wrapper {
-			background:white;
-			width:570px;
+			background:#e2e2e2;
+			/*width:570px;*/
 			margin:auto;
-			padding:10px 40px;
+			/*padding:10px 40px;*/
 			margin-bottom:40px;
 			margin-top:20px;
-			border-right: 1px solid #666666;
-			border-bottom: 1px solid #666666;
+			/*border-right: 1px solid #666666;*/
+			/*border-bottom: 1px solid #666666;*/
 		}
-		.elgg-messages-exception {
-			background:#FDFFC3;
-			display:block;
-			padding:10px;
+		h1{
+			border-bottom:none;
+		}
+		.img{
+			width:25%;
 		}
 		</style>
 
 	</head>
-	<body>
-	<div id="elgg-wrapper">
-		<h1><?php echo $vars['title']; ?></h1>
-		<?php echo $vars['body']; ?>
+	
+	<div id="elgg-wrapper" class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+		<h1><i class="fa fa-exclamation-circle" aria-hidden="true"></i> Sorry, this page could not be found (<?php echo $str ?>)</h1>
+		</div>
+		<div class="col-sm-12">The page "<?php echo $str?>" could not be found.</div>
+
+
+		<div class="col-sm-8">Try keyword search:</div>
+
+
+    <!-- <section id="wb-srch" class="col-xs-6 text-right visible-md visible-lg"> -->
+                <?php if ($gc_language === '' || $gc_language === 'en' || !$gc_language) { ?>
+        <form  action="http://intranet.canada.ca/search-recherche/query-recherche-eng.aspx" method="get" name="cse-search-box" class="form-inline mrgn-bttm-sm col-sm-6">
+        <?php } else { ?>
+        <form action="http://intranet.canada.ca/search-recherche/query-recherche-fra.aspx" method="get" name="cse-search-box" class="form-inline mrgn-bttm-sm col-sm-6">
+        <?php } ?>
+            <div class="form-group">
+                <label for="wb-srch-q" class="wb-inv">
+                    <?php echo elgg_echo('wet:searchweb');?>
+                </label>
+                <input class="wb-srch-q form-control" name="q"  value="" size="27" maxlength="150" placeholder="<?php echo elgg_echo('wet:searchgctools');?>">
+
+                <input type="hidden" name="a"  value="s">
+                <input type="hidden" name="s"  value="3">
+                <input type="hidden" name="chk4"  value="True">
+
+            </div>
+            <div class="form-group submit">
+                <!-- search button -->
+                <button type="submit" class="btn btn-primary btn-small" name="wb-srch-sub">
+                    <span class="glyphicon-search glyphicon"></span>
+                    <span class="wb-inv"> <?php echo elgg_echo('wet:searchHead');?> </span>
+                </button>
+            </div>
+        </form>
+    <!-- </section> -->
+
+    <img class="img" src='https://cdn4.iconfinder.com/data/icons/emoticons-outline/512/21-512.png' />
+
+</div>
 	</div>
-	</body>
-</html>
+	
+
